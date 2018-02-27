f44fd62ae9e20312aa422058438927e4781c005e
ygSW7 <3400294+ygSW7@users.noreply.github.com>
2018-02-27 16:34:51 +0000

    2nd

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+ // .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'jade')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
